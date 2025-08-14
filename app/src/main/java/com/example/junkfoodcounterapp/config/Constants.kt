859d3c5ccf5457836a04f868bdb929896ac7b694
Esmeralda Lopez <72100223+esme-lopez-r2devpros@users.noreply.github.com>
2025-08-13 20:35:59 -0600

    Create Constants.kt

---
+++ app/src/main/java/com/example/junkfoodcounterapp/config/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.junkfoodcounterapp.config
+
+object Constants {
+    val OPERATION_KEY = "OPERATION_KEY"
+    val OPERATION_INSERT = "OPERATION_INSERT"
+    val OPERATION_UPDATE = "OPERATION_UPDATE"
+}
